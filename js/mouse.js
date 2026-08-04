4faea835ebbaf1819c4ea721e5c32d874aa291be
ADarkHero <maik.riedlsperger@gmail.com>
2026-08-04 11:08:16 +0200

    Version 26.320 - You can pet the dog now Also small mobile bugfixes

---
+++ js/mouse.js
@@ -0,0 +1,43 @@
+var settingsLook = "default";
+
+document.addEventListener('mousemove', function(event) {
+    //Currently only supports setting picture in bottom right corner!
+    var windowWidth = window.innerWidth;
+    var windowHeight = window.innerHeight;
+
+    var settingsHeight = 128;
+    var settingsWidth = 128;
+
+    console.log(settingsLook);
+
+    //look left
+    if(event.clientY > windowHeight - settingsHeight && event.clientX < windowWidth - settingsWidth){
+        if(settingsLook !== "left"){
+            document.getElementById("settings-mops").src = "img/settings-mops-left.png";
+            settingsLook = "left";
+        }
+    }
+    else if(event.clientX < windowWidth - settingsWidth){
+            if(settingsLook !== "lefttop"){
+                document.getElementById("settings-mops").src = "img/settings-mops-lefttop.png";
+                settingsLook = "lefttop";
+            }
+    }
+    //look top
+    else if(event.clientX > windowWidth-settingsWidth && event.clientY < windowHeight - settingsHeight){
+        if(settingsLook !== "top"){
+            document.getElementById("settings-mops").src = "img/settings-mops-top.png";
+            settingsLook = "top";
+        }
+    }
+    else{
+        if(settingsLook !== "animation"){
+            document.getElementById("settings-mops").src = "img/settings-mops-animation.gif";
+            settingsLook = "animation";
+        }
+    }
+      
+    //Debug
+    //console.log('Mouse X:', event.clientX, 'Mouse Y:', event.clientY);
+    //console.log('Windowwith:', windowWidth, 'Settingswidth:', settingsWidth);
+});
